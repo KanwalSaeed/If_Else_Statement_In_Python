de525e40960c20357745e98f05b89da2c928a422
KanwalSaeed <kanwal_saeed122@hotmail.com>
2020-02-06 19:52:47 +0500

    first code

---
+++ 16. IF ELSE STATEMENT IN PYTHON.py
@@ -0,0 +1,12 @@
+
+
+balance = 300
+
+coupen_number = input("Enter your Coupen Number: ")
+if coupen_number =="1234":
+    balance += 250
+    print("Congratulation you have win price")
+    print("Your Balance is" +str(balance))
+else:
+    print("Sorry, You have enter an invalid Coupen Number")
+
